da19302384cd4b56cd13e55d46467aa978d78a56
Thiago Ramos da Silva <168379461+Thyagors@users.noreply.github.com>
2024-09-09 17:50:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "é uma skatista brasileira que conquistou o mundo com seu talento precoce. Conhecida como 'Fada', ela se tornou a mais jovem medalhista olímpica da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "é uma judoca brasileira, destaque em competições internacionais de judô. Ela é conhecida por sua força e técnica, representando o Brasil com grande orgulho em todos os tatames.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "é uma ginasta artística brasileira, medalhista olímpica e campeã mundial. Com seu talento único e dedicação, Rebeca se tornou um dos maiores nomes do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
+    }
+];
